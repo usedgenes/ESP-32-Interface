28328e6fa9d2fee4068ccad83dcbd027530c9d32
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-07-23 21:45:42 -0700

    Initial Commit

---
+++ ESP 32 Interface/ESP_32_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESP_32_InterfaceApp.swift
+//  ESP 32 Interface
+//
+//  Created by Eugene on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ESP_32_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ESP 32 InterfaceUITests/ESP_32_InterfaceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ESP_32_InterfaceUITestsLaunchTests.swift
+//  ESP 32 InterfaceUITests
+//
+//  Created by Eugene on 7/23/24.
+//
+
+import XCTest
+
+final class ESP_32_InterfaceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
