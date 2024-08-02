8493e1b4af3cd7acaa48d147b6e746e1e63e9214
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-08-02 13:19:52 -0700

    Edited Files

--- ESP 32 Interface/Bluetooth/BTDevice.swift
+++ ESP 32 Interface/Bluetooth/BTDevice.swift
@@ -25,7 +25,7 @@ class BTDevice: NSObject {
     private let manager: CBCentralManager
     private var blinkChar: CBCharacteristic?
     private var speedChar: CBCharacteristic?
-    private var _blink: Bool = false
+    public var _blink: Bool = false
     private var _speed: Int = 5
     
     weak var delegate: BTDeviceDelegate?
@@ -39,19 +39,6 @@ class BTDevice: NSObject {
             _blink = newValue
             if let char = blinkChar {
                 peripheral.writeValue(Data(bytes: [_blink ? 1 : 0]), for: char, type: .withResponse)
-            }
-        }
-    }
-    var speed: Int {
-        get {
-            return _speed
-        }
-        set {
-            guard _speed != newValue else { return }
-            
-            _speed = newValue
-            if let char = speedChar {
-                peripheral.writeValue(Data(bytes: [UInt8(_speed)]), for: char, type: .withResponse)
             }
         }
     }
@@ -103,9 +90,7 @@ extension BTDevice: CBPeripheralDelegate {
         print("Device: discovered services")
         peripheral.services?.forEach {
             print("  \($0)")
-            if $0.uuid == BTUUIDs.servo {
-                peripheral.discoverCharacteristics([], for: $0)
-            } else if $0.uuid == BTUUIDs.blinkService {
+            if $0.uuid == BTUUIDs.blinkService {
                 peripheral.discoverCharacteristics([BTUUIDs.blinkOn], for: $0)
             } else {
                 peripheral.discoverCharacteristics(nil, for: $0)
@@ -123,11 +108,6 @@ extension BTDevice: CBPeripheralDelegate {
                 self.blinkChar = $0
                 peripheral.readValue(for: $0)
                 peripheral.setNotifyValue(true, for: $0)
-            } else if $0.uuid == BTUUIDs.servo {
-                self.speedChar = $0
-                peripheral.readValue(for: $0)
-            } else if $0.uuid == BTUUIDs.servo {
-                peripheral.readValue(for: $0)
             }
         }
         print()
@@ -141,16 +121,6 @@ extension BTDevice: CBPeripheralDelegate {
         if characteristic.uuid == blinkChar?.uuid, let b = characteristic.value?.parseBool() {
             _blink = b
             delegate?.deviceBlinkChanged(value: b)
-        }
-        if characteristic.uuid == speedChar?.uuid, let s = characteristic.value?.parseInt() {
-            _speed = Int(s)
-            delegate?.deviceSpeedChanged(value: _speed)
-        }
-        if characteristic.uuid == BTUUIDs.servo, let d = characteristic.value {
-            serial = String(data: d, encoding: .utf8)
-            if let serial = serial {
-                delegate?.deviceSerialChanged(value: serial)
-            }
         }
     }
 }
