29c91ebc42156b4ff36810b184e6f2c6ca391784
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-08-06 10:27:46 -0700

    Edited Files

--- Arduino Code/Test/Test.ino
+++ Arduino Code/Test/Test.ino
@@ -8,13 +8,14 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
+#include "SparkFun_BNO08x_Arduino_Library.h"  
 
 #define SERVICE_UUID "9a8ca9ef-e43f-4157-9fee-c37a3d7dc12d"
 #define BLINK_UUID "e94f85c8-7f57-4dbd-b8d3-2b56e107ed60"
 #define SERVO_UUID "f74fb3de-61d1-4f49-bd77-419b61d188da"
-#define BMP390SPI_UUID "94cbc7dc-ff62-4958-9665-0ed477877581"
+#define BMP390_UUID "94cbc7dc-ff62-4958-9665-0ed477877581"
+#define BNO08X_UUID "c91b34b8-90f3-4fee-89f7-58c108ab198f"
 
-#define UUID_2 "c91b34b8-90f3-4fee-89f7-58c108ab198f"
 #define UUID_3 "21072f3b-b950-4b29-bf97-9a7be82d93e7"
 #define UUID_4 "78c611e3-0d36-491f-afe4-60ecc0c26a85"
 #define UUID_5 "6492bdaa-60e3-4e8a-a978-c923dec9fc37"
@@ -44,12 +45,17 @@ int servoArraySize;
 
 Adafruit_BMP3XX *bmp390Array;
 int bmp390ArraySize;
+TwoWire I2C = TwoWire(0);
+
+BNO08x *bno08xArray;
+int bno08xArraySize;
+SPIClass vspi = SPIClass(VSPI);
+TwoWire I2C1 = TwoWire(1);
 
 BLECharacteristic *pCharBlink;
 BLECharacteristic *pServo;
 BLECharacteristic *pBMP390;
-
-TwoWire I2C = TwoWire(0);
+BLECharacteristic *pBNO08X;
 
 void (*resetFunc)(void) = 0;  // create a standard reset function
 
@@ -170,21 +176,81 @@ class BMP390Callbacks : public BLECharacteristicCallbacks {
     }
     if (value.substring(0, 1) == "2") {
       String bmp390Number = value.substring(1,3);
-      bmp390Array[bmp390Numbe.toInt()].performReading();
+      bmp390Array[bmp390Number.toInt()].performReading();
       Serial.println("Writing bmp390s");
-      pCharacteristic->setValue("3" + bmp390Number + String(bmp390Array[value.substring(1,3).toInt()].temperature));
-      Serial.println(bmp390Array[value.substring(1,3).toInt()].temperature);
+      pCharacteristic->setValue(bmp390Number + "3" + String(bmp390Array[value.substring(1,3).toInt()].temperature));
       pCharacteristic->notify();
       delay(5);
-      pCharacteristic->setValue("4" + bmp390Number + String(bmp390Array[value.substring(1,3).toInt()].pressure));
+      pCharacteristic->setValue(bmp390Number + "4" + String(bmp390Array[value.substring(1,3).toInt()].pressure));
       pCharacteristic->notify();
-      Serial.println(bmp390Array[value.substring(1,3).toInt()].pressure);
       delay(5);
-      pCharacteristic->setValue("5" + bmp390Number + String(bmp390Array[value.substring(1,3).toInt()].readAltitude(SEALEVELPRESSURE_HPA)));
+      pCharacteristic->setValue(bmp390Number + "5" + String(bmp390Array[value.substring(1,3).toInt()].readAltitude(SEALEVELPRESSURE_HPA)));
       pCharacteristic->notify();
-      Serial.println(bmp390Array[value.substring(1,3).toInt()].readAltitude(SEALEVELPRESSURE_HPA));
     }
+    // Serial.println(value);
+  }
+};
 
+class BNO08XCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    String value = pCharacteristic->getValue();
+    if (value.substring(0, 1) == "0") {
+      bno08xArraySize = value.substring(1, value.length()).toInt();
+      bno08xArray = new BNO08x[bno08xArraySize];
+      Serial.println("Allocating bno08x");
+    }
+    if (value.substring(0, 1) == "1") {
+      if(value.length() == 7) {
+        int SDA = value.substring(3, 5).toInt();
+        int SCL = value.substring(5, 7).toInt();
+        I2C1.begin(SDA, SCL, 100000);
+        bno08xArray[value.substring(1, 3).toInt()].begin(0x4B, I2C1, -1, -1);
+        bno08xArray[value.substring(1, 3).toInt()].enableRotationVector();
+        delay(100);
+        bno08xArray[value.substring(1, 3).toInt()].enableGyro();
+        delay(100);
+        bno08xArray[value.substring(1, 3).toInt()].enableAccelerometer();;
+        delay(100);
+      }
+      //spi.begin: SCK, MISO, MOSI, CS
+      else {
+        vspi.begin(value.substring(3, 5).toInt(), value.substring(5, 7).toInt(), value.substring(7, 9).toInt(), value.substring(9, 11).toInt());
+        bno08xArray[value.substring(1, 3).toInt()].beginSPI(value.substring(11, 13).toInt(), value.substring(13, 15).toInt(), value.substring(15, 17).toInt(), 1000000, vspi);
+        bno08xArray[value.substring(1, 3).toInt()].enableRotationVector();
+        delay(100);
+        bno08xArray[value.substring(1, 3).toInt()].enableGyro();
+        delay(100);
+        bno08xArray[value.substring(1, 3).toInt()].enableAccelerometer();;
+        delay(100);
+      }
+      Serial.println("Attaching bno08xs");
+    }
+    if (value.substring(0, 1) == "2") {
+      String bno08xNumber = value.substring(1,3);
+      int bno08xNumberInt = bno08xNumber.toInt();
+      float quatI = bno08xArray[bno08xNumberInt].getQuatI();
+      float quatJ = bno08xArray[bno08xNumberInt].getQuatJ();
+      float quatK = bno08xArray[bno08xNumberInt].getQuatK();
+      float quatReal = bno08xArray[bno08xNumberInt].getQuatReal();
+      float quatRadianAccuracy = bno08xArray[bno08xNumberInt].getQuatRadianAccuracy();    
+
+      float x1 = bno08xArray[bno08xNumberInt].getGyroX();
+      float y1 = bno08xArray[bno08xNumberInt].getGyroY();
+      float z1 = bno08xArray[bno08xNumberInt].getGyroZ();
+
+      float x2= bno08xArray[bno08xNumberInt].getAccelX();
+      float y2 = bno08xArray[bno08xNumberInt].getAccelY();
+      float z2 = bno08xArray[bno08xNumberInt].getAccelZ();
+      
+      pCharacteristic->setValue(bno08xNumber + "3" + String(quatI, 2) + "," + String(quatJ, 2) + "," + String(quatK, 2) + "," + String(quatReal, 2) + "," + String(quatRadianAccuracy, 2));
+      pCharacteristic->notify();
+      delay(5);
+      pCharacteristic->setValue(bno08xNumber + "4" + String(x1, 2) + "," + String(y1, 2) + "," + String(z1, 2));
+      pCharacteristic->notify();
+      delay(5);
+      pCharacteristic->setValue(bno08xNumber + "5" + String(x2, 2) + "," + String(y2, 2) + "," + String(z2, 2));
+      pCharacteristic->notify();
+    }
     Serial.println(value);
   }
 };
@@ -204,7 +270,6 @@ void setup() {
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   pCharBlink = pService->createCharacteristic(BLINK_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE);
@@ -213,8 +278,11 @@ void setup() {
   pServo = pService->createCharacteristic(SERVO_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE);
   pServo->setCallbacks(new ServoCallbacks());
 
-  pBMP390 = pService->createCharacteristic(BMP390SPI_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE);
+  pBMP390 = pService->createCharacteristic(BMP390_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE);
   pBMP390->setCallbacks(new BMP390Callbacks());
+
+  pBNO08X = pService->createCharacteristic(BNO08X_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE);
+  pBNO08X->setCallbacks(new BNO08XCallbacks());
 
   pService->start();
 
