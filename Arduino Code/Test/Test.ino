7303ce16d5eb378f78db018fb074c7b41a21427f
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-08-06 17:55:13 -0700

    Edited Files

--- Arduino Code/Test/Test.ino
+++ Arduino Code/Test/Test.ino
@@ -8,7 +8,7 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
-#include "SparkFun_BNO08x_Arduino_Library.h"  
+#include "SparkFun_BNO08x_Arduino_Library.h"
 
 #define SERVICE_UUID "9a8ca9ef-e43f-4157-9fee-c37a3d7dc12d"
 #define BLINK_UUID "e94f85c8-7f57-4dbd-b8d3-2b56e107ed60"
@@ -154,7 +154,7 @@ class BMP390Callbacks : public BLECharacteristicCallbacks {
     }
     if (value.substring(0, 1) == "1") {
       //beginI2C: int address, twowire *wire
-      if(value.length() == 7) {
+      if (value.length() == 7) {
         int SDA = value.substring(3, 5).toInt();
         int SCL = value.substring(5, 7).toInt();
         I2C.begin(SDA, SCL, 100000);
@@ -175,18 +175,15 @@ class BMP390Callbacks : public BLECharacteristicCallbacks {
       Serial.println("Attaching bmp390s");
     }
     if (value.substring(0, 1) == "2") {
-      String bmp390Number = value.substring(1,3);
+      String bmp390Number = value.substring(1, 3);
       bmp390Array[bmp390Number.toInt()].performReading();
       Serial.println("Writing bmp390s");
-      pCharacteristic->setValue(bmp390Number + "3" + String(bmp390Array[value.substring(1,3).toInt()].temperature));
+      pCharacteristic->setValue(bmp390Number + "3" + String(bmp390Array[value.substring(1, 3).toInt()].temperature));
       pCharacteristic->notify();
-      delay(15);
-      pCharacteristic->setValue(bmp390Number + "4" + String(bmp390Array[value.substring(1,3).toInt()].pressure));
+      pCharacteristic->setValue(bmp390Number + "4" + String(bmp390Array[value.substring(1, 3).toInt()].pressure));
       pCharacteristic->notify();
-      delay(15);
-      pCharacteristic->setValue(bmp390Number + "5" + String(bmp390Array[value.substring(1,3).toInt()].readAltitude(SEALEVELPRESSURE_HPA)));
+      pCharacteristic->setValue(bmp390Number + "5" + String(bmp390Array[value.substring(1, 3).toInt()].readAltitude(SEALEVELPRESSURE_HPA)));
       pCharacteristic->notify();
-      delay(15);
     }
     // Serial.println(value);
   }
@@ -201,60 +198,87 @@ class BNO08XCallbacks : public BLECharacteristicCallbacks {
       Serial.println("Allocating bno08x");
     }
     if (value.substring(0, 1) == "1") {
-      if(value.length() == 7) {
+      if (value.length() == 7) {
         int SDA = value.substring(3, 5).toInt();
         int SCL = value.substring(5, 7).toInt();
         I2C1.begin(SDA, SCL, 100000);
         bno08xArray[value.substring(1, 3).toInt()].begin(0x4B, I2C1, -1, -1);
         bno08xArray[value.substring(1, 3).toInt()].enableRotationVector();
-        delay(100);
         bno08xArray[value.substring(1, 3).toInt()].enableGyro();
         delay(100);
-        bno08xArray[value.substring(1, 3).toInt()].enableAccelerometer();;
-        delay(100);
+        bno08xArray[value.substring(1, 3).toInt()].enableAccelerometer();
+        Serial.println("began i2c");
       }
       //spi.begin: SCK, MISO, MOSI, CS
       else {
         vspi.begin(value.substring(3, 5).toInt(), value.substring(5, 7).toInt(), value.substring(7, 9).toInt(), value.substring(9, 11).toInt());
-        bno08xArray[value.substring(1, 3).toInt()].beginSPI(value.substring(3, 5).toInt(), value.substring(11, 13).toInt(), value.substring(13, 15).toInt(), 1000000, vspi);
-        bno08xArray[value.substring(1, 3).toInt()].enableRotationVector();
-        delay(100);
-        bno08xArray[value.substring(1, 3).toInt()].enableGyro();
-        delay(100);
-        bno08xArray[value.substring(1, 3).toInt()].enableAccelerometer();;
-        delay(100);
+        bno08xArray[value.substring(1, 3).toInt()].beginSPI(value.substring(9, 11).toInt(), value.substring(11, 13).toInt(), value.substring(13, 15).toInt(), 1000000, vspi);
+        delay(50);
+        setReports(value.substring(1, 3).toInt());
+        Serial.println("Attaching bno08xs");
       }
-      Serial.println("Attaching bno08xs");
     }
     if (value.substring(0, 1) == "2") {
-      String bno08xNumber = value.substring(1,3);
+      Serial.println("Received");
+      String bno08xNumber = value.substring(1, 3);
       int bno08xNumberInt = bno08xNumber.toInt();
-      float quatI = bno08xArray[bno08xNumberInt].getQuatI();
-      float quatJ = bno08xArray[bno08xNumberInt].getQuatJ();
-      float quatK = bno08xArray[bno08xNumberInt].getQuatK();
-      float quatReal = bno08xArray[bno08xNumberInt].getQuatReal();
-      float quatRadianAccuracy = bno08xArray[bno08xNumberInt].getQuatRadianAccuracy();    
+      if (bno08xArray[bno08xNumberInt].wasReset()) {
+        Serial.println("sensor was reset");
+        setReports(bno08xNumberInt);
+      }
+      if (bno08xArray[bno08xNumberInt].getSensorEvent() == true) {}
+      // float quatI = bno08xArray[bno08xNumberInt].getQuatI();
+      // float quatJ = bno08xArray[bno08xNumberInt].getQuatJ();
+      // float quatK = bno08xArray[bno08xNumberInt].getQuatK();
+      // float quatReal = bno08xArray[bno08xNumberInt].getQuatReal();
 
       float x1 = bno08xArray[bno08xNumberInt].getGyroX();
       float y1 = bno08xArray[bno08xNumberInt].getGyroY();
       float z1 = bno08xArray[bno08xNumberInt].getGyroZ();
 
-      float x2= bno08xArray[bno08xNumberInt].getAccelX();
+      float x2 = bno08xArray[bno08xNumberInt].getAccelX();
       float y2 = bno08xArray[bno08xNumberInt].getAccelY();
       float z2 = bno08xArray[bno08xNumberInt].getAccelZ();
-      
-      pCharacteristic->setValue(bno08xNumber + "3" + String(quatI, 2) + "," + String(quatJ, 2) + "," + String(quatK, 2) + "," + String(quatReal, 2) + "," + String(quatRadianAccuracy, 2));
-      pCharacteristic->notify();
-      delay(5);
+
+      // pCharacteristic->setValue(bno08xNumber + "3" + String(quatI, 2) + ",");
+      // pCharacteristic->notify();
+      // Serial.println("done");
       pCharacteristic->setValue(bno08xNumber + "4" + String(x1, 2) + "," + String(y1, 2) + "," + String(z1, 2));
       pCharacteristic->notify();
-      delay(5);
+      Serial.println(bno08xNumber + "4" + String(x1, 2) + "," + String(y1, 2) + "," + String(z1, 2));
       pCharacteristic->setValue(bno08xNumber + "5" + String(x2, 2) + "," + String(y2, 2) + "," + String(z2, 2));
       pCharacteristic->notify();
+      Serial.println(bno08xNumber + "4" + String(x1, 2) + "," + String(y1, 2) + "," + String(z1, 2));
+      Serial.println("notifying bno08x");
     }
-    Serial.println(value);
   }
+  Serial.println(value);
 };
+
+void setReports(int bno08xNumberInt) {
+  if (bno08xArray[bno08xNumberInt].enableRotationVector() == true) {
+    Serial.println(F("Rotation vector enabled"));
+    Serial.println(F("Output in form i, j, k, real, accuracy"));
+  } else {
+    Serial.println("Could not enable rotation vector");
+  }
+  delay(100);
+
+  if (bno08xArray[bno08xNumberInt].enableGyro() == true) {
+    Serial.println(F("Gyro enabled"));
+    Serial.println(F("Output in form x, y, z, in radians per second"));
+  } else {
+    Serial.println("Could not enable gyro");
+  }
+  delay(100);
+
+  if (bno08xArray[bno08xNumberInt].enableAccelerometer() == true) {
+    Serial.println(F("Accelerometer enabled"));
+  } else {
+    Serial.println("Could not enable accelerometer");
+  }
+  delay(100);
+}
 
 void setup() {
   Serial.begin(115200);
