de77a1b98c9a1dcbc15f9192931174657a9bdc91
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-08-07 12:59:56 -0700

    Edited Files

--- ESP 32 Interface/Views/AppHelpView.swift
+++ ESP 32 Interface/Views/AppHelpView.swift
@@ -1,22 +1,93 @@
 import SwiftUI
 import PDFKit
+import UIKit
 
 struct AppHelpView: View {
+    @State private var isSharePresented: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Button(action: {
-            saveFile()
-        }) {
-            Text("Save File")
+        VStack{
+            Text("App Info")
+                .font(.largeTitle)
+                .padding()
+                
+            CollapsibleView(
+                label: { Text("Collapsible") },
+                content: {
+                    HStack {
+                        Text("ContentContentContentContentContentContentContentContentContent")
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                }
+            )
+            Button("Share app") {
+                self.isSharePresented = true
+            }
+            .sheet(isPresented: $isSharePresented, onDismiss: {
+                print("Dismiss")
+            }, content: {
+                let url = Bundle.main.url(forResource: "test", withExtension: "pdf")!
+                var document = PDFDocument(url: url)
+                let documentData = document!.dataRepresentation()
+                ActivityViewController(activityItems: ["Name To Present to User", documentData], applicationActivities: nil)
+            })
         }
     }
+}
+
+struct ActivityViewController: UIViewControllerRepresentable {
     
-    func saveFile() {
-        let url = Bundle.main.url(forResource: "test", withExtension: "pdf")!
-        var document = PDFDocument(url: url)
-        guard let documentData = document!.dataRepresentation() else { return }
-        let activityController = UIActivityViewController(activityItems: [documentData], applicationActivities: nil)
-        self.present(activityController, animated: true, completion: nil)
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityViewController>) {}
+    
+}
+
+struct CollapsibleView<Content: View>: View {
+    @State var label: () -> Text
+    @State var content: () -> Content
+    
+    @State private var collapsed: Bool = true
+    
+    var body: some View {
+        VStack {
+            Button(
+                action: { self.collapsed.toggle() },
+                label: {
+                    HStack {
+                        self.label()
+                            .padding(.leading)
+                            .font(.title2)
+                        Spacer()
+                        Image(systemName: self.collapsed ? "chevron.down" : "chevron.up")
+                            .padding(.trailing)
+                    }
+                    .padding(.bottom, 1)
+                    .background(Color.white.opacity(0.01))
+                }
+            )
+            .buttonStyle(PlainButtonStyle())
+            
+            Divider()
+                .frame(height: 0)
+                .padding(.leading)
+                .padding(.trailing)
+            
+            VStack {
+                self.content()
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: collapsed ? 0 : .none)
+            .clipped()
+            .offset(y: -15)
+        }
     }
 }
 
